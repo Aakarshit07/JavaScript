8ae5487d1d4ac2da9d8a98aac1eda0132ac6c3eb
Aakarshit Choudhary <102687269+Aakarshit07@users.noreply.github.com>
2023-09-28 11:48:45 +0530

    Todo - Project 1

---
+++ Projects/Todo App/script.js
@@ -0,0 +1,46 @@
+const subHeading = document.getElementById("sub-heading");
+const list = document.getElementById("todo-list");
+const submitButton = document.getElementById("btn");
+const deletebutton = document.querySelector(".delete-btn");
+
+const formattedDate = new Date().toLocaleDateString("en-US", {
+  weekday: "long",
+  year: "numeric",
+  month: "short",
+  day: "numeric"
+});
+subHeading.textContent = formattedDate;
+// console.log(subHeading);
+
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const todoInput = document.getElementById("todo-input").value;
+
+  if (todoInput.trim() !== "") {
+    const listItem = document.createElement("div");
+    listItem.className = "todo";
+    listItem.innerHTML = `
+    <li class="list-group-item p-3">
+      <input class="form-check-input me-1" id="todo-1" type="checkbox" />
+      <label class="form-check-label" for="todo-1">${todoInput}</label>
+    </li>
+  `;
+
+    list.appendChild(listItem);
+    todoInput.value = "";
+  }
+});
+
+// delete button
+
+function clearAll() {
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+}
+
+deletebutton.addEventListener("click", function (event) {
+  event.preventDefault();
+  clearAll();
+});
